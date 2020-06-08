7bfd0b5035051c0fc34094f6a0c2688f0cec4129
Sushmitha A T <48562432+sushmithaat@users.noreply.github.com>
2020-06-08 20:57:09 +0530

    Create number.cpp

---
+++ Even or Odd Number/number.cpp
@@ -0,0 +1,20 @@
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin>>n;
+
+    if(n%2==0)
+        cout<<"Even Number\n";
+    else
+        cout<<"Odd Number\n";
+
+    return 0;
+}
+
+
