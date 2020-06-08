9bd6a1d0c21aef734ed6da65cd5d04574fa9ee0d
Sushmitha A T <48562432+sushmithaat@users.noreply.github.com>
2020-06-08 20:35:18 +0530

    Create swap.cpp

---
+++ swap.cpp
@@ -0,0 +1,21 @@
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+
+    cin>>a;
+    cin>>b;
+
+    a=a+b;
+    b=a-b;
+    a=a-b;
+
+    cout<<"a="<<a<<" "<<"b="<<b;
+
+    return 0;
+}
+
