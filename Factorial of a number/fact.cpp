03400b2ea5e0c38c7984d846ddba50c5832799ee
Sushmitha A T <48562432+sushmithaat@users.noreply.github.com>
2020-06-08 20:19:16 +0530

    Create fact.cpp

---
+++ Factorial of a number/fact.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,fact=1,i;
+
+    cin>>n;
+
+    for(i=1;i<=n;i++)
+    {
+        fact=fact*i;
+    }
+    cout<<fact;
+
+    return 0;
+}
+
