0fe9df8dc6a282dcd4f37e78c2a0ac6ea80fd892
Sushmitha A T <48562432+sushmithaat@users.noreply.github.com>
2020-06-08 21:12:29 +0530

    Create char.cpp

---
+++ A character is a vowel or consonant/char.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    char ch;
+
+    cin>>ch;
+
+    if(ch=='a' ||ch=='e' ||ch=='i' ||ch=='o' ||ch=='u' ||ch=='A' ||ch=='E' ||ch=='I' ||ch=='O' ||ch=='U')
+    {
+        cout<<"Vowel"<<endl;
+    }
+    else
+    {
+        cout<<"Consonant"<<endl;
+    }
+    return 0;
+}
+
+
+
