0fd6666a50d7624ee9eb7bc68b0ab44efd088c71
Sushmitha A T <48562432+sushmithaat@users.noreply.github.com>
2020-06-08 21:28:49 +0530

    Create pow.cpp

---
+++ Power of a Number/pow.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+    int base,exp,res;
+
+    cin>>base;
+    cin>>exp;
+
+    res=pow(base,exp);
+
+    cout<<res;
+
+    return 0;
+}
+
+
+
